bc494636dfa305a30498eb5f42764b409ab1a666
Per Ragnar Edin <perragnar@gmail.com>
2021-02-25 17:49:12 +0100

    Initial commit

---
+++ generators/app/index.js
@@ -0,0 +1,102 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const slug = require('slug');
+const glob = require('glob');
+
+module.exports = class extends Generator {
+  async prompting() {
+    // Have Yeoman greet the user.
+    this.log(
+      yosay(`${chalk.red('Parcel Web App')} generator!`)
+    );
+
+    // Initial promp
+    const initPrompt = [
+      {
+        type: 'input',
+        name: 'appTitleInput',
+        message: `Enter the ${chalk.blue('title')} of your web app.`,
+        default: 'Parcel Web App'
+      },
+      {
+        type: 'checkbox',
+        name: 'options',
+        message: `Initialize a ${chalk.blue('Git repository')}?`,
+        choices: [
+          {
+            name: `Initialize a ${chalk.blue('Git repository')}`,
+            value: 'initGit',
+            checked: false
+          },
+          {
+            name: `Open project in ${chalk.blue('Visual Studio Code')}?`,
+            value: 'startVSCode',
+            checked: false
+          },
+          {
+            name: `Start ${chalk.blue('Parcel dev server')}?`,
+            value: 'startDevServer',
+            checked: true
+          },
+        ],
+        store: true
+      }
+    ];
+
+    return this.prompt(initPrompt).then(initAnswer => {
+      this.props = {
+        title: initAnswer.appTitleInput,
+        appTitle: slug(initAnswer.appTitleInput, { lower: true }),
+        options: initAnswer.options
+      };
+    });
+  }
+
+  default() {
+  }
+
+  writing() {
+    this.log(`${chalk.green('Copying files.')}`);
+
+    this.fs.copyTpl(
+      glob.sync(this.templatePath('{/**/*,*,.*}'), { dot: true }),
+      this.destinationPath(),
+      // Template data
+      {
+        title: this.props.title,
+        appTitle: this.props.appTitle
+      }
+    );
+  }
+
+  install() {
+    // Installing dependencies
+    this.log(`${chalk.green('Installing dependencies.')}`  );
+    this.installDependencies({ bower: false });
+  }
+
+  end() {
+    // Initialize git repository
+    if (this.props.options.includes('initGit')) {
+      this.log(`${chalk.green('Initializing Git repository.')}`);
+      this.spawnCommand('git', ['init']);
+    //   this.spawnCommand('git', ['add', '.']);
+    //   this.spawnCommand('git', ['commit', '-m', '"Initial commit"']);
+    //   this.spawnCommand('git', ['pull', 'git@github.com:perragnar/website-starterkit-parcel.git']);
+    }
+
+    // Start VSCode
+    if (this.props.options.includes('startVSCode')) {
+      this.log(`${chalk.green('Open project in Visual Studio Code.')}`);
+      this.spawnCommand('code', ['.']);
+    }
+
+    // Start development server
+    if (this.props.options.includes('startDevServer')) {
+      this.log(`${chalk.green('Starting the dev server.')}`);
+      this.spawnCommand('npm', ['run', 'dev']);
+    }
+  }
+};
---
+++ generators/app/templates/README.md
@@ -0,0 +1,54 @@
+# Website Starter Kit (Parcel edition)
+
+A simple HTML, Javascript, SCSS boilerplate with Parcel.
+
+## Requirements
+
+- [Node.js](https://nodejs.org/en/) and [npm](https://www.npmjs.com/)
+- [Git](https://git-scm.com/)
+
+## Instructions
+
+### Step 1: Create a new project folder
+
+Create the new site folder where ever you want on your computer and `cd` into it.
+
+### Step 2: Initialize a new Git repository
+
+Instead of cloning this repository, create an empty one locally.
+
+```
+git init
+```
+
+Then pull this repository into your newly created one.
+
+```
+git pull git@github.com:perragnar/website-starterkit-parcel.git
+```
+
+### Step 3: Install the Node packages
+
+All the required packages needs to be installed.
+
+```
+npm install
+```
+
+### Step 4: Build and serve
+
+In the package.json file there are two scripts defined. One for running a dev server with browser sync, and the other is to build the site for deployment.
+
+To run the develop server, just run
+
+```
+npm run dev
+```
+
+To build the site for deployment run
+
+```
+npm run build
+```
+
+**Happy hacking!**
